0e0f4c6411d31b0ecc9edd7f1e8cf2de6e5f390e
Angshuman Gupta <angshu.gupta789@gmail.com>
2024-07-17 18:46:47 +0200

    Create script.js
    added script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+// Function to load YAML file
+async function loadYamlFile(file) {
+    const response = await fetch(file);
+    const yamlText = await response.text();
+    return jsyaml.load(yamlText);
+}
+
+// Function to populate products
+function populateProducts(productsConfig) {
+    const productsContainer = document.getElementById('products');
+    for (const [key, product] of Object.entries(productsConfig)) {
+        const productDiv = document.createElement('div');
+        productDiv.className = 'product';
+        productDiv.innerHTML = `
+            <a href="${product.url}" target="_blank">
+                <i class="${product.icon}"></i>
+                <span>${product.name}</span>
+            </a>
+            <div class="users">${product.users} active users</div>
+        `;
+        productsContainer.appendChild(productDiv);
+    }
+}
+
+// Load YAML and populate products when the page loads
+window.onload = async function() {
+    const productsConfig = await loadYamlFile('products.yaml');
+    populateProducts(productsConfig);
+};
