ddaebf9a2b92116bf5be1f91d1e429b424e1591b
angshu-min-js <angshu.gupta789@gmail.com>
2025-05-25 10:16:37 +0200

    CNAME

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,10 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    copyPublicDir: true
+  }
 }));
